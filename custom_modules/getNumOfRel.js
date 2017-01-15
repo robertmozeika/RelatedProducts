3139ee12551824c887a5832eb4a0c41e95d079cf
robertmozeika <robertmozeika20@gmail.com>
2017-01-14 22:34:48 -0500

    fixing getNumofRel

--- custom_modules/getNumOfRel.js
+++ custom_modules/getNumOfRel.js
@@ -1,103 +1,93 @@
 var mongodb = require('mongodb');
+const rpModel = require('../models/relatedProducts.js')
+const spModel = require('../models/storeProducts.js')
+
 
 function getRelatedProducts(inp, shop){
   return new Promise(function(resolve, reject){
- var MongoClient = mongodb.MongoClient;
+     rpModel.find({"store":shop}).sort('order')
+      .then(function(result){
+          console.log('$$',result)
+         var rpPass = {};
+         if (result !== undefined){
+          console.log("connection successful at getRel")
+          result.forEach((element)=>{
+            if (rpPass[element.forProduct]){
+              rpPass[element.forProduct].push(element.title)
 
- var url = "mongodb://robertm:testpass@ds155418.mlab.com:55418/relatedproducts"
- MongoClient.connect(url, function(err, db){
-   if(err){
-     console.log('Unable to connect' + err)
-   } else {
-     console.log('Connection between Database Success');
+            }
+            else {
+              rpPass[element.forProduct] = [element.title];
+            }
 
-     var collection = db.collection('RelatedProducts');
-     collection.find({"store":shop},{"sort": "order"}).toArray(function(err, result){
-       var rpPass = {};
-       if(err) {
+          })
 
-        console.log(err)
-      }else if (result !== undefined){
-        console.log("connection successful at getRel")
-        result.forEach((element)=>{
-          if (rpPass[element.forProduct]){
-            rpPass[element.forProduct].push(element.title)
-
-          }
-          else {
-            rpPass[element.forProduct] = [element.title];
-          }
-
-        })
-
-        inp.forEach((ele)=>{
-          ele.relatedProducts = rpPass[ele.productID];
-        })
-        resolve(rpPass);
+          inp.forEach((ele)=>{
+            ele.relatedProducts = rpPass[ele.productID];
+          })
+          resolve(rpPass);
 
 
+
+         }
+       else {
+         console.log('passing nothing at getRel');
+
+         var passPromise = [res, []]
+
+         resolve(passPromise)
 
        }
-     else {
-       console.log('passing nothing at getRel');
 
-       var passPromise = [res, []]
 
-       resolve(passPromise)
+     }).catch(function(err){
+       console.log('Err at getRelatedProducts in getNumOfRel.js',err);
+     })
 
-     }
-
-     db.close();
-
-   });
-
-   }
- })
 });
 
 }
 
 function getNumOfRel(res, shop){
   return new Promise(function(resolve, reject){
- var MongoClient = mongodb.MongoClient;
+ // var MongoClient = mongodb.MongoClient;
+ //
+ // var url = "mongodb://robertm:testpass@ds155418.mlab.com:55418/relatedproducts"
+ // MongoClient.connect(url, function(err, db){
+ //   if(err){
+ //     console.log('Unable to connect' + err)
+ //   } else {
+ //     console.log('Connection between Database Success');
+ //
+ //     var collection = db.collection("StoreProducts");
 
- var url = "mongodb://robertm:testpass@ds155418.mlab.com:55418/relatedproducts"
- MongoClient.connect(url, function(err, db){
-   if(err){
-     console.log('Unable to connect' + err)
-   } else {
-     console.log('Connection between Database Success');
+     spModel.find({"store":shop})
+      .then((result)=>{
+         if (result !== undefined){
+           console.log('##',result)
+          getRelatedProducts(result, shop).then(function(inp){
+            var passPromise = [res, result, inp, shop]
 
-     var collection = db.collection("StoreProducts");
+            resolve(passPromise)
+          })
 
-     collection.find({"store":shop}).toArray(function(err, result){
-       if(err) {
 
-        console.log(err)
-      }else if (result !== undefined){
-        console.log("connection successful at getNumOfRel")
-        getRelatedProducts(result, shop).then(function(inp){
-          var passPromise = [res, result, inp, shop]
+         }
+       else {
+         console.log('passing nothing at getNumRel')
+         var passPromise = [res, []]
 
-          resolve(passPromise)
-        })
-
+         resolve(passPromise)
 
        }
-     else {
-       console.log('passing nothing at getNumRel')
-       var passPromise = [res, []]
 
-       resolve(passPromise)
 
-     }
+     }).catch(err=>{
+       console.log('Err at getNumOfRel in getNumOfRel.js',err);
+     });//
 
-     db.close();
-
-   });
-
-   }
- })
+ //   }
+ // })
 });
 
 }
--- custom_modules/renderMain.js
+++ custom_modules/renderMain.js
@@ -4,18 +4,12 @@ var addProducts2DB = require('./addsProducts2DB.js');
 var getDefaultNum = require('./getDefaultNum.js');
 
 function renderPromises(res,shop,shopify){
-
-
   Promise.all([
     getNumOfRel(res, shop),
     getShop(shopify),
     getDefaultNum(shop)
-
-
   ]).then(function(values){
     return addProducts2DB(values, shop, shopify)
-
-
   }).then(function(values){
     // console.log(values)
         console.log(values[2])
