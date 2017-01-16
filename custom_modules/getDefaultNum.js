c21f96d01f49fd61b6fecdde864e2fc75c62d586
robertmozeika <robertmozeika20@gmail.com>
2017-01-16 12:43:25 -0500

    cleaned up getDefaultNum.js

--- custom_modules/getDefaultNum.js
+++ custom_modules/getDefaultNum.js
@@ -1,50 +1,29 @@
-var mongodb = require('mongodb');
+const shopModel = require('../models/shops.js');
 
 function getDefault(shop){
   return new Promise(function(resolve, reject){
-  var numOfArr = [];
- var MongoClient = mongodb.MongoClient;
-
- var url = "mongodb://robertm:testpass@ds155418.mlab.com:55418/relatedproducts"
- MongoClient.connect(url, function(err, db){
-   if(err){
-     console.log('Unable to connect' + err)
-   } else {
-     console.log('Connection between Database Success');
-
-     var collection = db.collection('shops');
-
-     collection.find({"name": shop}).toArray(function(err, result){
-       if(err) {
-
-        console.log(err)
-      }else if (result !== undefined){
-         var defaultNum = result[0].defaultNumOfRelated;
-         var allMostBought = result[0].allMostBought;
-         var header = result[0].header;
-         var defNumPass = {
-           defaultNum: defaultNum,
-           allMostBought: allMostBought,
-           header: header,
+     shopModel.find({"name": shop})
+       .then(result=>{
+         if (result.length > 0){
+             var defaultNum = result[0].defaultNumOfRelated;
+             var allMostBought = result[0].allMostBought;
+             var header = result[0].header;
+             var defNumPass = {
+               defaultNum: defaultNum,
+               allMostBought: allMostBought,
+               header: header,
+             }
+             resolve(defNumPass)
          }
-
-         resolve(defNumPass)
-
-       }
-     else {
-       console.log('passing nothing at getNumRel')
-
-       resolve(3)
-
-     }
-
-     db.close();
-
-   });
-
-   }
- })
-});
+         else {
+           console.log('passing nothing at getNumRel')
+           //3 is the default chosen for new stores
+           resolve(3)
+         }
+       }).catch(err=>{
+         console.log("Error at getDefaultNum from renderMain ", err)
+       });
+  });
 
 }
 
--- custom_modules/getShopifyData.js
+++ custom_modules/getShopifyData.js
@@ -1,14 +1,4 @@
 var shopifyAPI = require('shopify-node-api');
-var shopModel = require('../models/shops.js');
-
-function findWithAttr(array, attr, value) {
-    for(var i = 0; i < array.length; i += 1) {
-        if(array[i][attr] === value) {
-            return i;
-        }
-    }
-    return -1;
-}
 
 //called from renderMain()
 //[0] returns array of products from shopify with all collections added as a property in an array, [1] is array of all collections
@@ -89,7 +79,7 @@ class GetShopData {
       const collectionMap = this.createMap(custom_collections,smart_collections);
       const collectionArray = this.createArray(collectionMap);
       collects.forEach((collect)=>{
-        var idx = findWithAttr(products,'id',collect.product_id);
+        var idx = this.findWithAttr(products,'id',collect.product_id);
         console.log(collect.collection_id,collectionMap.get(collect.collection_id),collectionMap)
         if (products[idx].collections){
           products[idx].collections.push(collectionMap.get(collect.collection_id))
@@ -119,122 +109,16 @@ class GetShopData {
     })
     return collectionArray;
   }
-
-}
-
-
-
-function prodFromShopify(shopifyconfig){
-  return new Promise(function(resolve, reject){
-    // if (resolve) {
-      var shopify = new shopifyAPI(shopifyconfig)
-
-      function getProducts(){
-        return new Promise((resolve,reject)=>{
-          shopify.get('/admin/products.json',  function(err, data, headers){
-                  // var stringdata = JSON.stringify(data.products);
-                        // resolve(JSON.parse(stringdata))
-                        resolve(data.products)
-
-                        if (err){
-                          console.log("reject is " + err)
-                          reject(err);
-                        }
-          });
-        })
-      }
-
-      function getCollects(){
-        return new Promise((resolve,reject)=>{
-          shopify.get('/admin/collects.json', function(err, data, headers){
-            // console.log(data)
-                  // var stringdata = JSON.stringify(data.products);
-                  console.log(data)
-                        resolve(data.collects)
-
-                        if (err){
-                          console.log("reject is " + err)
-                          reject(err);
-                        }
-          });
-        })
-      }
-
-      function getCustomCollections(){
-        return new Promise((resolve,reject)=>{
-          shopify.get('/admin/custom_collections.json', function(err, data, headers){
-            // console.log(data)
-                  // var stringdata = JSON.stringify(data.products);
-                        resolve(data.custom_collections)
-
-                        if (err){
-                          console.log("reject is " + err)
-                          reject(err);
-                        }
-          });
-        })
-      }
-
-      function getSmartCollections(){
-        return new Promise((resolve,reject)=>{
-          shopify.get('/admin/smart_collections.json', function(err, data, headers){
-            // console.log(data)
-                  // var stringdata = JSON.stringify(data.products);
-                        resolve(data.smart_collections)
-
-                        if (err){
-                          console.log("reject is " + err)
-                          reject(err);
-                        }
-          });
-        })
-      }
-
-      Promise.all([
-        getProducts(),
-        getCustomCollections(),
-        getSmartCollections(),
-        getCollects(),
-
-      ]).then(([ products, custom_collections, smart_collections, collects ])=>{
-        var collectionMap = new Map();
-
-        custom_collections.forEach((element)=>{
-          collectionMap.set(element.id,element.title)
-        })
-
-        smart_collections.forEach((element)=>{
-          collectionMap.set(element.id,element.title)
-        })
-
-        const collectionArray = [];
-        collectionMap.forEach((val)=>{
-        collectionArray.push({value: val, display: val})
-        })
-
-
-        collects.forEach((collect)=>{
-          var idx = findWithAttr(products,'id',collect.product_id);
-          console.log(collect.collection_id,collectionMap.get(collect.collection_id),collectionMap)
-          if (products[idx].collections){
-            products[idx].collections.push(collectionMap.get(collect.collection_id))
-          } else {
-            products[idx].collections = [collectionMap.get(collect.collection_id)]
-            products[idx].collections = products[idx].collections.sort()
+  findWithAttr(array, attr, value) {
+      for(var i = 0; i < array.length; i += 1) {
+          if(array[i][attr] === value) {
+              return i;
           }
-        })
-
-
-        // console.log('collections',collections);
-        resolve([products,collectionArray])
-      })
-
-      // }
-
-
-
-    });
+      }
+      return -1;
+  }
 
 }
-// module.exports = prodFromShopify;
+
+
 module.exports = GetShopData
