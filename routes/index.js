395fb7ff9264dcf367e0df44a4b57c9e1be8137d
robertmozeika <robertmozeika20@gmail.com>
2017-01-17 15:32:18 -0500

    handled index

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ var authorize = require('../custom_modules/authorize.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //saves shop name for later use with accessing database
-  req.session.shop = req.query.shop.replace('.myshopify', "").replace('.com', "");
-  console.log('session: ',req.session)
-  authorize.connectShop(req.session.shop, req, res)
-
+  if (req.query.shop){
+    req.session.shop = req.query.shop.replace('.myshopify', "").replace('.com', "");
+    console.log('session: ',req.session)
+    authorize.connectShop(req.session.shop, req, res)
+  } else {
+    res.send('Please go to /install')
+  }
 });
 
 module.exports = router;
